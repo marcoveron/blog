6b625b3305ec61f0d874cc3d1799f18f4e29cd24
Marco Verón <mv.marcoveron@gmail.com>
2026-04-22 16:13:26 +0800

    Add google analytics with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://marcoveron.me',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), partytown({config: { forward: ['dataLayer.push']}})],
 	fonts: [
 		{
 			provider: fontProviders.local(),
